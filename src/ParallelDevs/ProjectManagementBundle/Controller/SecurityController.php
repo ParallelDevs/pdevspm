c05b4002f84fc78da02aef3657562ec4c47f82f5
Daniel Noyola <danielnv18@gmail.com>
2015-08-19 07:15:08 -0600

    Created security controller
    Added Path, controller and view

---
+++ src/ParallelDevs/ProjectManagementBundle/Controller/SecurityController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ParallelDevs\ProjectManagementBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+    public function loginAction()
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(
+          'security/login.html.twig',
+          array(
+              // last username entered by the user
+            'last_username' => $lastUsername,
+            'error'         => $error,
+          )
+        );
+    }
+
+    public function loginCheckAction()
+    {
+        // will never be executed
+    }
+}
