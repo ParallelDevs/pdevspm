aad5aa331bf315ae913a3ce6231fefe36851687a
Daniel Noyola <danielnv18@gmail.com>
2015-09-01 21:18:40 -0600

    Added Security configs.

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,14 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: danielnv18
- * Date: 9/1/15
- * Time: 9:12 PM
- */
 
 namespace AppBundle\Entity;
 
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
 
-class User {
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+  /**
+   * @ORM\Id
+   * @ORM\Column(type="integer")
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  protected $id;
 
+  public function __construct()
+  {
+    parent::__construct();
+  }
 }
