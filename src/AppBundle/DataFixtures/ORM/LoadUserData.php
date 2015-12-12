a20e770165aefb994ef3faf308fae05929c45012
danielnv18 <danielnv18@gmail.com>
2015-12-11 20:52:52 -0600

    Added fixtures.

---
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use AppBundle\Entity\User;
+
+class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        // Create an admin user for test only
+        if ($this->container->getParameter('kernel.environment') == 'test') {
+            $adminUser = new User();
+            $adminUser
+                ->addRole('ROLE_ADMIN')
+                ->setUsername('admin')
+                ->setUsernameCanonical('admin')
+                ->setEmail('admin@pdevspm.com')
+                ->setEmailCanonical('admin@pdevspm.com')
+                ->setEnabled(true)
+                ->setPlainPassword('admin')
+            ;
+
+            $manager->persist($adminUser);
+            $manager->flush();
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrder()
+    {
+        return 1;
+    }
+}
