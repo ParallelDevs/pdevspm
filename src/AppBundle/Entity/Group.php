9603e866cbbaae2422f66683e6c44dffc2d90cdf
Daniel Noyola <danielnv18@gmail.com>
2015-12-24 12:12:03 -0600

    Added Fos group.

---
+++ src/AppBundle/Entity/Group.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danielnv18
+ * Date: 12/24/15
+ * Time: 11:50 AM
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\Group as BaseGroup;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_group")
+ */
+class Group extends BaseGroup
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
