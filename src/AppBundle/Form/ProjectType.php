006e10d49a26f9cc19e9fcde8192648a65883307
Daniel Noyola <danielnv18@gmail.com>
2016-02-23 11:28:33 -0600

    Generated CRUD for Project.

---
+++ src/AppBundle/Form/ProjectType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjectType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+            ->add('description')
+            ->add('createdAt', 'datetime')
+            ->add('orderTaskBy')
+            ->add('team')
+            ->add('projectStatus')
+            ->add('projectType')
+            ->add('createdBy')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Project'
+        ));
+    }
+}
