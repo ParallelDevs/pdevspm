be65e48b2605d67f7db580667bf949c3de80c9d1
Daniel Noyola <danielnv18@gmail.com>
2016-05-02 07:25:38 -0600

    Updated fixtures.

--- src/AppBundle/DataFixtures/ORM/LoadUserGroupData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUserGroupData.php
@@ -13,48 +13,22 @@ class LoadUserGroupData extends AbstractFixture implements OrderedFixtureInterfa
     {
         $admin = new UserGroup('Admin');
         $admin->addRole('ROLE_SUPER_ADMIN');
-        $admin
-            ->setViewAll(1)
-            ->setManageProjects(1)
-            ->setManageTasks(1)
-            ->setManageTickets(1)
-            ->setManageUser(1)
-            ->setManageConfiguration(1)
-            ->setManageDiscussions(1)
-        ;
         $manager->persist($admin);
         $manager->flush();
 
         $developer = new UserGroup('Developer');
-        $developer
-            ->setManageProjects(1)
-            ->setManageTasks(1)
-        ;
         $manager->persist($developer);
         $manager->flush();
 
         $client = new UserGroup('Client');
-        $client
-            ->setManageTickets(1)
-        ;
         $manager->persist($client);
         $manager->flush();
 
         $manager_group = new UserGroup('Manager');
-        $manager_group
-            ->setViewAll(1)
-            ->setManageProjects(1)
-            ->setManageTasks(1)
-            ->setManageTickets(1)
-            ->setManageUser(1)
-        ;
         $manager->persist($manager_group);
         $manager->flush();
 
         $designer = new UserGroup('Designer');
-        $designer
-            ->setManageTasks(1)
-        ;
         $manager->persist($designer);
         $manager->flush();
     }
