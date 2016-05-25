0743fdbf23d9761a0d9292f6262b5d32eb7ae9ad
danielnv18 <danielnv18@gmail.com>
2016-05-24 22:14:48 -0600

    Added watch task.

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,21 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', function () {
-    gulp.src('app/Resources/assets/sass/**/*.scss')
+var config = {
+    assetsDir: 'app/Resources/assets',
+    sassPattern: 'sass/**/*.scss'
+};
+
+gulp.task('sass', function () {
+    gulp.src(config.assetsDir+'/'+config.sassPattern)
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('web/css'));
 });
+
+gulp.task('watch', function () {
+    gulp.watch(config.assetsDir+'/'+config.sassPattern, ['sass'])
+});
+
+gulp.task('default', ['sass', 'watch']);
