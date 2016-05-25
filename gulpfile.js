c24661ec7192ba1b8cdab9c9f6a51bb5d21f92b7
danielnv18 <danielnv18@gmail.com>
2016-05-24 22:06:08 -0600

    Added sourcemaps.

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,12 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function () {
     gulp.src('app/Resources/assets/sass/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass())
-        .pipe(gulp.dest('web/css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('web/css'));
 });
