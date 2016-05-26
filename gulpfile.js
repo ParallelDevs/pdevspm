14126bdafeb2664b5810dae9529f47bb56d871cd
danielnv18 <danielnv18@gmail.com>
2016-05-25 21:33:41 -0600

    Added min task.

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
+var util = require('gulp-util');
 
 var config = {
     assetsDir: 'app/Resources/assets',
-    sassPattern: 'sass/**/*.scss'
+    sassPattern: 'sass/**/*.scss',
+    prod: !!util.env.prod
 };
 
 gulp.task('sass', function () {
@@ -15,6 +18,7 @@ gulp.task('sass', function () {
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(concat('main.css'))
+        .pipe(config.prod ? cleanCSS() : util.noop())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('web/css'));
 });
