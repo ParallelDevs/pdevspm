c0be4ef30678763314165297335dcbde467f1735
danielnv18 <danielnv18@gmail.com>
2016-05-25 21:02:26 -0600

    Added concat task.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
 
 var config = {
     assetsDir: 'app/Resources/assets',
@@ -13,6 +14,7 @@ gulp.task('sass', function () {
     gulp.src(config.assetsDir+'/'+config.sassPattern)
         .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(concat('main.css'))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('web/css'));
 });
