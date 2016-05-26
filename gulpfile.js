04f785315d73724281681b0098ccd14a24040c27
danielnv18 <danielnv18@gmail.com>
2016-05-25 21:45:07 -0600

    Added some magic to load the gulp plugins.

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,28 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var concat = require('gulp-concat');
-var cleanCSS = require('gulp-clean-css');
-var util = require('gulp-util');
-var gulpif = require('gulp-if');
+// var sass = require('gulp-sass');
+// var sourcemaps = require('gulp-sourcemaps');
+// var concat = require('gulp-concat');
+// var cleanCSS = require('gulp-clean-css');
+// var util = require('gulp-util');
+// var gulpif = require('gulp-if');
+var plugins = require('gulp-load-plugins')();
 
 var config = {
     assetsDir: 'app/Resources/assets',
     sassPattern: 'sass/**/*.scss',
-    prod: !!util.env.prod,
-    sourceMaps: !util.env.prod
+    prod: !!plugins.util.env.prod,
+    sourceMaps: !plugins.util.env.prod
 };
 
 gulp.task('sass', function () {
     gulp.src(config.assetsDir+'/'+config.sassPattern)
-        .pipe(gulpif(config.sourceMaps, sourcemaps.init()))
-        .pipe(sass())
-        .pipe(concat('main.css'))
-        .pipe(config.prod ? cleanCSS() : util.noop())
-        .pipe(gulpif(config.sourceMaps, sourcemaps.write('.')))
+        .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.init()))
+        .pipe(plugins.sass())
+        .pipe(plugins.concat('main.css'))
+        .pipe(config.prod ? plugins.cleanCss() : plugins.util.noop())
+        .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.write('.')))
         .pipe(gulp.dest('web/css'));
 });
 
