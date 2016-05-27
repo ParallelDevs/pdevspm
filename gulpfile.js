672b17a1dd0ca83b9f29d75b2d07ef13b164261d
danielnv18 <danielnv18@gmail.com>
2016-05-26 22:12:42 -0600

    Added script task.

--- gulpfile.js
+++ gulpfile.js
@@ -13,26 +13,45 @@ var config = {
 
 var app = {};
 
-app.styles = function (paths, outputFilename) {
+app.addStyles = function (paths, filename) {
     gulp.src(paths)
         .pipe(plugins.plumber())
         .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.init()))
         .pipe(plugins.sass())
-        .pipe(plugins.concat(outputFilename))
+        .pipe(plugins.concat(filename))
         .pipe(config.prod ? plugins.cleanCss() : plugins.util.noop())
         .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.write('.')))
         .pipe(gulp.dest('web/css'));
 };
 
+app.addScripts = function (paths, filename) {
+    gulp.src(paths)
+        .pipe(plugins.plumber())
+        .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.init()))
+        .pipe(plugins.concat(filename))
+        .pipe(config.prod ? plugins.uglify() : plugins.util.noop())
+        .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.write('.')))
+        .pipe(gulp.dest('web/js'));
+};
+
 gulp.task('styles', function () {
-    app.styles([
+    app.addStyles([
         config.bowerDir + '/bootstrap/dist/css/bootstrap.css',
         config.assetsDir+'/'+config.sassPattern
     ], 'main.css');
 });
 
-gulp.task('watch', function () {
-    gulp.watch(config.assetsDir+'/'+config.sassPattern, ['styles'])
+gulp.task('scripts', function () {
+    app.addScripts([
+        config.bowerDir+'/jquery/dist/jquery.js',
+        config.bowerDir+'/bootstrap/dist/js/bootstrap.js',
+        config.assetsDir+'/js/main.js'
+    ], 'app.js');
 });
 
-gulp.task('default', ['styles', 'watch']);
+gulp.task('watch', function () {
+    gulp.watch(config.assetsDir+'/'+config.sassPattern, ['styles']);
+    gulp.watch(config.assetsDir+'/js/**/*.js', ['scripts']);
+});
+
+gulp.task('default', ['styles', 'scripts', 'watch']);
