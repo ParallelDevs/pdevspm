41b210489849dc6aac8ee29cb382d6521b17f4d3
danielnv18 <danielnv18@gmail.com>
2016-05-26 21:56:04 -0600

    Added bower components.

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,35 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
 var config = {
-    assetsDir: 'app/Resources/assets',
+    assetsDir : 'app/Resources/assets',
+    bowerDir : 'vendor/bower_components',
     sassPattern: 'sass/**/*.scss',
     prod: !!plugins.util.env.prod,
     sourceMaps: !plugins.util.env.prod
 };
 
-gulp.task('sass', function () {
-    gulp.src(config.assetsDir+'/'+config.sassPattern)
+var app = {};
+
+app.styles = function (paths, outputFilename) {
+    gulp.src(paths)
         .pipe(plugins.plumber())
         .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.init()))
         .pipe(plugins.sass())
-        .pipe(plugins.concat('main.css'))
+        .pipe(plugins.concat(outputFilename))
         .pipe(config.prod ? plugins.cleanCss() : plugins.util.noop())
         .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.write('.')))
         .pipe(gulp.dest('web/css'));
+};
+
+gulp.task('styles', function () {
+    app.styles([
+        config.bowerDir + '/bootstrap/dist/css/bootstrap.css',
+        config.assetsDir+'/'+config.sassPattern
+    ], 'main.css');
 });
 
 gulp.task('watch', function () {
-    gulp.watch(config.assetsDir+'/'+config.sassPattern, ['sass'])
+    gulp.watch(config.assetsDir+'/'+config.sassPattern, ['styles'])
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['styles', 'watch']);
