4ed547f29162af2d5f8896293958bf5269c578d5
Daniel Noyola <danielnv18@gmail.com>
2016-06-25 14:27:57 -0600

    Better theme layout

--- src/AppBundle/Menu/MenuBuilder.php
+++ src/AppBundle/Menu/MenuBuilder.php
@@ -20,7 +20,29 @@ class MenuBuilder
     {
         $menu = $this->factory->createItem('root');
 
-        $menu->addChild('Home', array('route' => 'homepage'));
+        $menu->setChildrenAttribute('class', 'sidebar-menu');
+        // Header
+        $menu->addChild('MAIN NAVIGATION');
+        $menu['MAIN NAVIGATION']->setAttribute('class', 'header');
+
+        // Project
+        $menu->addChild('project', [
+            'label' => '<i class="fa fa-sitemap"></i> <span>Projects</span> <i class="fa fa-angle-left pull-right"></i>',
+            'uri' => '#',
+            'extras' => ['safe_label' => true],
+        ]);
+        $menu['project']->setAttribute('class', 'treeview');
+        $menu['project']->setChildrenAttribute('class', 'treeview-menu');
+        $menu['project']->addChild('list', [
+          'label' => '<i class="fa fa-circle-o"></i> View All',
+          'route' => 'project_index',
+          'extras' => ['safe_label' => true],
+        ]);
+        $menu['project']->addChild('New', [
+          'label' => '<i class="fa fa-circle-o"></i> Add Project',
+          'route' => 'project_new',
+          'extras' => ['safe_label' => true],
+        ]);
 
         return $menu;
     }
